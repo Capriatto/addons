b21608016afb0a02b0502be69b4766f11a438fea
Sandra Figueroa Varela <sandrafigvar@gmail.com>
2016-06-22 09:32:30 +0200

    [fix]

--- sales_state/models/sale_order.py
+++ sales_state/models/sale_order.py
@@ -5,7 +5,8 @@ from openerp import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    state = fields.Selection(selection_add=[('valorar', "Para valorar"),('valorado', "Valorado")])
+    state = fields.Selection(selection_add=[('valorar', "Para valorar")])
+    #state = fields.Selection(selection_add=[('valorar', "Para valorar"),('valorado', "Valorado")])
 
 #    @api.one
 #    def action_quotation_approve(self):
@@ -14,10 +15,10 @@ class SaleOrder(models.Model):
     @api.multi
     def action_quotation_valorar(self):   	
         self.write({'state': 'valorar'})
-        return True
 
-    @api.multi
-    def action_quotation_valorado(self):   	
-        self.write({'state': 'valorado'})
-        return True
+
+ #   @api.multi
+ #   def action_quotation_valorado(self):   	
+ #       self.write({'state': 'valorado'})
+
         
