060b1ce03850d1d5658ac36bacaffdaea507936b
Sandra Figueroa Varela <sandrafigvar@gmail.com>
2016-07-07 12:25:15 +0200

    fix

--- calendar_event_meeting_reason/models/calendar_event.py
+++ calendar_event_meeting_reason/models/calendar_event.py
@@ -10,8 +10,8 @@ class CalendarEvent(models.Model):
         string="Meeting reason",
         ondelete="restrict")
 
-    class CalendarEventMeetingReason(models.Model):
-        _name = 'calendar.event.meeting.reason'
-        _description = 'Calendar Event Meeting Reason'
+class CalendarEventMeetingReason(models.Model):
+    _name = 'calendar.event.meeting.reason'
+    _description = 'Calendar Event Meeting Reason'
 
-        name = fields.Char('Reason', required=False, translate=True)
+    name = fields.Char('Reason', required=False, translate=True)
